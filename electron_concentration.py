05d9fcea16ccc9abc37fc425319cc3448f84b9ce
Muhammad Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-06-30 16:35:45 +0500

    Update electron_concentration.py

--- electron_concentration.py
+++ electron_concentration.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt 
 
-ND = [0, 1E10, 2E10, 3E10, 4E10, 5E10] 
-final_ND=[]
-ni = 1.90189E10
+ND = [0, 1E10, 2E10, 3E10, 4E10, 5E10]                 #concentration of donor atom (Phosphorus). [carrier/cubic cm]
+final_ND = []
+ni = 1.90189E10                                        #concentration of intrinsic silicon at room temperature. [carrier/cubic cm]
 for each in ND:
-    s = each + ni
+    s = each + ni                                      #concentration of electron in n-type doping. [carrier/cubic cm]
     final_ND.append(s)
 nn = final_ND
 plt.xlabel('Concentration of the Donor atoms.')  
 plt.ylabel('Concentration of Electrons.') 
 plt.plot(ND, nn, label = "Curve")   
 plt.savefig("Electron_Concentration.png")
-plt.legend() 
-plt.show() 
+plt.legend()
+plt.show()
