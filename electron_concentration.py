b9f9f90af60d79346aa45651398a9519b935930c
Muhammad Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-06-30 16:03:51 +0500

    Update electron_concentration.py

--- electron_concentration.py
+++ electron_concentration.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt 
 
-ND = [0,1E10,2E10,3E10,4E10,5E10] 
+ND = [0, 1E10, 2E10, 3E10, 4E10, 5E10] 
 final_ND=[]
 ni = 1.90189E10
 for each in ND:
-    s=each+ni
+    s = each + ni
     final_ND.append(s)
-nn=final_ND
+nn = final_ND
 plt.xlabel('Concentration of the Donor atoms.')  
 plt.ylabel('Concentration of Electrons.') 
 plt.plot(ND, nn, label = "Curve")   
