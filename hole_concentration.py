9b5c428116e71ae41ef7169e60738d9f96b2770d
Muhammad Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-06-30 16:35:54 +0500

    Update hole_concentration.py

--- hole_concentration.py
+++ hole_concentration.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 ni = 1.90189E10                                       #concentration of intrinsic silicon at room temperature. [carrier/cubic cm]
-ND = [0, 1E10, 2E10, 3E10, 4E10, 5E10]                #donor atom concentration (Phosphorus). [carrier/cubic cm]
+ND = [0, 1E10, 2E10, 3E10, 4E10, 5E10]                #concentration of donor atom (Phosphorus). [carrier/cubic cm]
 values = []
 for x in ND:
     np = (ni*ni)/(x + ni)                             #concentration of holes in n-type doping. [carrier/cubic cm]
@@ -12,4 +12,4 @@ plt.xlabel('Concentration of the Donor atoms.')
 plt.ylabel('Concentration of Holes.')  
 plt.legend() 
 plt.savefig("Holes_Concentration.png")
-plt.show() 
+plt.show()
