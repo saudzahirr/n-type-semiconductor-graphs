14e085b35572cb3b4bf74814807c0a958c10b588
Muhammad Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-06-30 16:22:25 +0500

    Update resistivity_concentration.py

--- resistivity_concentration.py
+++ resistivity_concentration.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 
-q = 1.6E-19
-ni = 1.90189E10
-up = 480
-un = 1350
-ND = [1E15, 2E15, 3E15, 4E15, 5E15]
+q = 1.6E-19                                                    #charge of electron.
+ni = 1.90189E10                                                #concentration of intrinsic silicon at room temperature. [carrier/cubic cm]
+up = 480                                                       #holes mobility.    [square cm/V.s]
+un = 1350                                                      #electron mobility. [square cm/V.s]
+ND = [1E15, 2E15, 3E15, 4E15, 5E15]                            #donor atom concentration (Phosphorus). [carrier/cubic cm]
 values=[]
 for x in ND:
-    p = (x + ni)/(q*((un*x*x) + (un*ni*x) + (up*ni*ni)))
+    p = (x + ni)/(q*((un*x*x) + (un*ni*x) + (up*ni*ni)))       #'p' is resistivity of doped semiconductor. [ohm.cm]
     values.append(p)
 plt.plot(ND, values, label = "Curve") 
 plt.xlabel('Concentration of the Donor atoms.')  
