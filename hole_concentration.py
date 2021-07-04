79f834cc1b1e5c475ad0d29f3bc856f24c07573d
Muhammad Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-07-04 16:08:56 +0500

    Update hole_concentration.py

--- hole_concentration.py
+++ hole_concentration.py
@@ -1,15 +1,16 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 
-ni = 1.90189E10                                       #concentration of intrinsic silicon at room temperature. [carrier/cubic cm]
-ND = [0, 1E10, 2E10, 3E10, 4E10, 5E10]                #concentration of donor atom (Phosphorus). [carrier/cubic cm]
+ni = 1.90189E10                                                 #concentration of intrinsic silicon at room temperature. [carrier/cubic cm]
+ND = [1E10, 2E10, 3E10, 4E10, 5E10, 6E10, 7E10, 8E10, 9E10]     #concentration of donor atom (Phosphorus). [carrier/cubic cm]
 values = []
 for x in ND:
-    np = (ni*ni)/(x + ni)                             #concentration of holes in n-type doping. [carrier/cubic cm]
+    np = (ni**2)/(x + ni)                                       #concentration of holes in n-type doping. [carrier/cubic cm]
     values.append(np)
-plt.plot(ND, values, label = "Curve")  
-plt.xlabel('Concentration of the Donor atoms.')  
-plt.ylabel('Concentration of Holes.')  
+plt.plot(ND, values)
+plt.title(r"$n_{p} = \frac{ n_{i}^{2} }{ N_{D} + n_{i} }$")
+plt.xlabel('Concentration of the Donor atoms. [carrier/cubic cm]')  
+plt.ylabel('Concentration of Holes. [carrier/cubic cm]')  
 plt.legend() 
 plt.savefig("Holes_Concentration.png")
 plt.show()
