79870a8368621399384fce5de2ce1515eb80b92c
Muhammad Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-07-04 16:20:52 +0500

    Update resistivity_concentration.py

--- resistivity_concentration.py
+++ resistivity_concentration.py
@@ -5,14 +5,15 @@ q = 1.6E-19                                                    #charge of electr
 ni = 1.90189E10                                                #concentration of intrinsic silicon at room temperature. [carrier/cubic cm]
 up = 480                                                       #holes mobility.    [square cm/V.s]
 un = 1350                                                      #electron mobility. [square cm/V.s]
-ND = [1E15, 2E15, 3E15, 4E15, 5E15]                            #concentration of donor atom (Phosphorus). [carrier/cubic cm]
+ND = [1E15, 2E15, 3E15, 4E15, 5E15, 6E15, 7E15, 8E15, 9E15]    #concentration of donor atom (Phosphorus). [carrier/cubic cm]
 values=[]
 for x in ND:
-    p = (x + ni)/(q*((un*x*x) + (un*ni*x) + (up*ni*ni)))       #'p' is resistivity of n-type doped semiconductor. [ohm.cm]
+    p = (x + ni)/(q*((un*x**2) + (un*ni*x) + (up*ni**2)))       #'p' is resistivity of n-type doped semiconductor. [ohm.cm]
     values.append(p)
-plt.plot(ND, values, label = "Curve") 
-plt.xlabel('Concentration of the Donor atoms.')  
-plt.ylabel('Resistivity of Silicon.') 
+plt.plot(ND, values)
+plt.title(r"$\rho = \frac{ N_{D} + n_{i} }{q \left( u_{n} \cdot N_{D}^{2} + u_{n} \cdot n_{i} \cdot N_{d} + u_{p} \cdot n_{i}^{2} \right) }$")
+plt.xlabel('Concentration of the Donor atoms. [carrier/cubic cm]')  
+plt.ylabel('Resistivity of Silicon. [ohm.cm]') 
 plt.legend() 
 plt.savefig("Restivity_Concentration.png")
 plt.show()
