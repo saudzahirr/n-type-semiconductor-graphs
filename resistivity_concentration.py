a9d87f357a1984e8697c6265a6211f8dee7d32c6
Muhammad Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-07-04 16:21:14 +0500

    Update resistivity_concentration.py

--- resistivity_concentration.py
+++ resistivity_concentration.py
@@ -8,7 +8,7 @@ un = 1350                                                      #electron mobilit
 ND = [1E15, 2E15, 3E15, 4E15, 5E15, 6E15, 7E15, 8E15, 9E15]    #concentration of donor atom (Phosphorus). [carrier/cubic cm]
 values=[]
 for x in ND:
-    p = (x + ni)/(q*((un*x**2) + (un*ni*x) + (up*ni**2)))       #'p' is resistivity of n-type doped semiconductor. [ohm.cm]
+    p = (x + ni)/(q*((un*x**2) + (un*ni*x) + (up*ni**2)))      #'p' is resistivity of n-type doped semiconductor. [ohm.cm]
     values.append(p)
 plt.plot(ND, values)
 plt.title(r"$\rho = \frac{ N_{D} + n_{i} }{q \left( u_{n} \cdot N_{D}^{2} + u_{n} \cdot n_{i} \cdot N_{d} + u_{p} \cdot n_{i}^{2} \right) }$")
